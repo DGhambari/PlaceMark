71c35dc96ce6b42c92aaf75a464ab3572c6367f7
Darren Ghambari <54907156+DGhambari@users.noreply.github.com>
2022-03-30 18:56:41 +0200

    Deployment fixes

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,30 @@
+export const seedData = {
+  users: {
+    _model: "User",
+    homer: {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret",
+      _id: "621f2bf90f8832d1b1b1234a",
+      __v: 0,
+    },
+    marge: {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret",
+      _id: "621f2bf90f8832d1b1b3630b",
+      __v: 0,
+    },
+    bart: {
+      admin: false,
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret",
+      _id: "621f2bf90f8832d1b1b3634a",
+      __v: 0,
+    }
+  },
+};
